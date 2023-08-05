f28c89625e750cb9de7b0ab171bc76f4804a652c
GhaniBahri <bahri.ghanou@gmail.com>
2023-08-05 15:02:00 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, name){
+    return arr.filter(el=> (el.toLowerCase() === name.toLowerCase()))
+}
+
+function fuzzyMatch(arr ,str){
+    return arr.filter(el=> el.startsWith(str))
+}
+
+function matchName(arr, name){
+    return arr.filter(el=>(el.name === name))
+}
